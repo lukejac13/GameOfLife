4a0096571529f0774c7103cbc52c02fd6e7c3c6a
Robert Utterback <robertu13@gmail.com>
2024-02-27 12:22:16 -0600

    basic learning

---
+++ game.js
@@ -0,0 +1,5 @@
+let container = document.getElementById('gameContainer');
+let p = document.createElement('p');
+p.innerHTML = 'Hello, world!';
+// As if `p` is `<p>Hello, world!</p>`
+container.appendChild(p);
